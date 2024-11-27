2a2f3e438e3d0ea8f990e5d9187a48a9dece6dc0
Brinley Ostler <brinley.ostler@gmail.com>
2024-11-26 20:12:57 -0700

    add first formula

--- main.py
+++ main.py
@@ -9,3 +9,16 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from my_plots import *
 import streamlit as st
+
+@st.cache_data
+
+# Load the data
+def load_books_data():
+    url = 'https://raw.githubusercontent.com/brinleyostler/AZ-top-books/main/AZ_top_books.csv'
+    df = pd.read_csv(url)
+
+    # Ebook and Audiobook data
+    df_e = df[df['Format'] == 'EBOOK']
+    df_a = df[df['Format'] == 'AUDIOBOOK']
+
+    return df, df_e, df_a
--- my_plots.py
+++ my_plots.py
@@ -3,3 +3,20 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import matplotlib.pyplot as plt
 import pandas as pd
+
+def alphabet_rating_df(books, letter='A'):
+    ebooks = books[books['Format'] == 'EBOOK'].copy()
+    audiobooks = books[books['Format'] == 'AUDIOBOOK'].copy()
+
+    # Return the highest rated books that start with the letter
+    books_letter = books[books['Title'].str.startswith(letter)]
+    ebooks_letter = books_letter[books_letter['Format'] == 'EBOOK']
+    ebooks_letter = ebooks_letter.sort_values(by=['Format', 'Rating'], ascending=False)
+
+    audiobooks_letter = books_letter[books_letter['Format'] == 'AUDIOBOOK']
+    audiobooks_letter = audiobooks_letter.sort_values(by=['Format', 'Rating'], ascending=False)
+
+    df_e = ebooks_letter.head(5)
+    df_a = audiobooks_letter.head(5)
+
+    return df_e, df_a
